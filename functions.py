2f4cb502983fb41aacb6e0429c29b78f6b0052c4
ErickGBR <erickburgos1519@gmail.com>
2024-10-11 14:30:47 -0600

    feat: functions and import

---
+++ functions.py
@@ -0,0 +1,8 @@
+#create function to square
+
+def square(x):
+    return x * x
+
+#square in range
+for i in range(10):
+    print(f"square of {i} is {square(i)}")
---
+++ squares.py
@@ -0,0 +1,11 @@
+from functions import square
+import functions
+
+#square in range with first methodto import
+for i in range(10):
+    print(f"square of {i} is {square(i)}")
+
+
+    #square in range
+for i in range(10):
+    print(f"square of {i} is {functions.square(i)}")
