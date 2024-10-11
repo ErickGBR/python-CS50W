305de8d1819e43c0c3684528626e0d8c8970892a
ErickGBR <erickburgos1519@gmail.com>
2024-10-11 09:29:45 -0600

    feat: practice loops and sets

---
+++ loops.py
@@ -0,0 +1,15 @@
+
+#each array with numbers
+for i in [0,1,2,3,4,5,6,]:
+    print(i)
+
+
+#each range data to get iteration
+for i in range(6):
+    print(i)
+
+
+#each text in array
+names = ["Harry", "Ron", "Hermione"]
+for i in names:
+    print(i)
