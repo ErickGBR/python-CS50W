c15ba5bfe8fdae2042d56fd62714ef04b8711db6
ErickGBR <erickburgos1519@gmail.com>
2024-10-13 19:50:06 -0600

    feat: how use exceptions from inputs

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,20 @@
 import sys
 #exceptions method to manage operations
-x = int(input("x: "))
-y = int(input("y: "))
 
+#validate input entries
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("")
+    sys.exit(1)
+
+
+#validate if divide by 0 exept the error
 try:
     result = x / y 
 except ZeroDivisionError:
     print("Error: cannot divide by 0.")
     sys.exit(1)
-    
+
 print(f"{x} / {y} = {result}")
